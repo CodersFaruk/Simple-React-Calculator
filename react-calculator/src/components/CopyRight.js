7aeb9c5c4eace2c4b48d477f119cf422a139dce8
CoderFaruk <66289933+CodersFaruk@users.noreply.github.com>
2022-04-12 19:11:19 +0600

    add Copyright and add a Components calculator.js

---
+++ react-calculator/src/components/CopyRight.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const CopyRight = () => {
+  return (
+    <>
+      <div className="rights">
+        <p>&copy;2021-{new Date().getFullYear()} CoderFaruk All Right Reserved.</p>
+      </div>
+    </>
+  );
+};
+
+export default CopyRight;
