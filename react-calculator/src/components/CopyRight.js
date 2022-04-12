30d7c4821e891ed99708ff129631d4439bdb46a6
CoderFaruk <66289933+CodersFaruk@users.noreply.github.com>
2022-04-12 19:20:39 +0600

    formate all file and complete project

--- react-calculator/src/components/CopyRight.js
+++ react-calculator/src/components/CopyRight.js
@@ -4,7 +4,9 @@ const CopyRight = () => {
   return (
     <>
       <div className="rights">
-        <p>&copy;2021-{new Date().getFullYear()} CoderFaruk All Right Reserved.</p>
+        <p>
+          &copy;2021-{new Date().getFullYear()} CoderFaruk All Right Reserved.
+        </p>
       </div>
     </>
   );
