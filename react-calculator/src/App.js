9d9d2ab4a575cfda33a7862073a4dd6f94ea3073
CoderFaruk <66289933+CodersFaruk@users.noreply.github.com>
2022-04-12 16:50:53 +0600

    opeation all complete

--- react-calculator/src/App.js
+++ react-calculator/src/App.js
@@ -1,35 +1,100 @@
+import React,{useState} from "react";
 import "./App.css";
 
+
 function App() {
+  const [current,setCurrent]=useState('');
+  const [privious,setPrivious]=useState('');
+  const [operation,setOperation]=useState('');
+
+  const appendValueHandler=(e)=>{
+    let value=e.target.getAttribute('data')
+    if(value==='.' && current.includes('.'))return
+    setCurrent(current+value)
+  }
+
+  const deleteHandler=()=>{
+    setCurrent(String(current).slice(0,-1))
+  }
+  const allDeleteHandler=()=>{
+    setCurrent('')
+    setPrivious('')
+    setOperation('')
+  }
+  const operationHandler=(e)=>{
+   if(current==='') return
+   if(privious !== ''){
+     let values=compute()
+     setPrivious(values)
+   }else{
+     setPrivious(current)
+   }
+   setCurrent('')
+   setOperation(e.target.getAttribute('data'))
+  }
+
+
+  const finalResult=()=>{
+    let value=compute()
+    if(value=== undefined || value=== null)return
+
+    setCurrent(value)
+    setPrivious('')
+    setOperation('')
+  }
+
+  const compute=()=>{
+    let result;
+    let priviousNumber=parseFloat(privious)
+    let currentNumber=parseFloat(current)
+    if(isNaN(priviousNumber) || isNaN(currentNumber)) return
+    switch(operation){
+      case '/': result=priviousNumber / currentNumber;
+      break;
+      case '*': result=priviousNumber * currentNumber;
+      break;
+      case '+': result=priviousNumber + currentNumber;
+      break;
+      case '-': result=priviousNumber - currentNumber;
+      break;
+      default: return
+    }
+    return result
+    
+  }
+
+
+
   return (
     <>
       <h2 className="header">Calculator</h2>
       <div className="main-container">
         <div className="screen">
-          <input type="text" disabled />
+          <div className="privious">{privious} {operation}</div>
+          <div className="current">{current}</div>
         </div>
 
         <div className="grid-container">
-          <div className="ac items">AC</div>
+          <div className="ac items" onClick={allDeleteHandler}>AC</div>
           <div className="m items">m+</div>
           <div className="m items">m-</div>
-          <div className="operator items">/</div>
-          <div className="decimal items">7</div>
-          <div className="decimal items">8</div>
-          <div className="decimal items">9</div>
-          <div className="operator items">&times;</div>
-          <div className="decimal items">4</div>
-          <div className="decimal items">5</div>
-          <div className="decimal items">6</div>
-          <div className="operator items">+</div>
-          <div className="decimal items">1</div>
-          <div className="decimal items">2</div>
-          <div className="decimal items">3</div>
-          <div className="operator items">-</div>
-          <div className="decimal items">0</div>
-          <div className="decimal items">.</div>
-          <div className="ac items">c</div>
-          <div className="equel items">=</div>
+          <div data={'/'} className="operator items" onClick={operationHandler}>/</div>
+          <div data={7} className="decimal items" onClick={appendValueHandler}>7</div>
+          <div data={8} className="decimal items" onClick={appendValueHandler}>8</div>
+          <div data={9} className="decimal items" onClick={appendValueHandler}>9</div>
+          <div data={'*'} className="operator items" onClick={operationHandler}>&times;</div>
+          <div data={4} className="decimal items" onClick={appendValueHandler}>4</div>
+          <div data={5} className="decimal items" onClick={appendValueHandler}>5</div>
+          <div data={6} className="decimal items" onClick={appendValueHandler}>6</div>
+          <div data={'+'} className="operator items" onClick={operationHandler}>+</div>
+          <div data={1} className="decimal items" onClick={appendValueHandler}>1</div>
+          <div data={2} className="decimal items" onClick={appendValueHandler}>2</div>
+          <div data={3} className="decimal items" onClick={appendValueHandler}>3</div>
+          <div data={'-'} className="operator items" onClick={operationHandler}>-</div>
+          <div data={0} className="decimal items" onClick={appendValueHandler}>0</div>
+          <div data={'.'} className="decimal items" onClick={appendValueHandler}>.</div>
+          <div  className="ac items" onClick={deleteHandler}>c</div>
+          <div className="equel items" onClick={finalResult}>=</div>
         </div>
       </div>
     </>
